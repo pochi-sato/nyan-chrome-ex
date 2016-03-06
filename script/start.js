2bfcb8173f44c98bd2270b7f5171d641eb78c78f
Takuto Sato <takuto@Tacknyan.local>
2016-03-06 20:44:46 +0900

    add music 'fox'

--- script/start.js
+++ script/start.js
@@ -2,6 +2,9 @@ var musics = {
   samba:function(){
     document.body.appendChild(new Audio('https://s3-ap-northeast-1.amazonaws.com/pochi-sozai/nyan-bunch/samba.mp3')).play();
   },
+  fox:function(){
+    document.body.appendChild(new Audio('https://s3-ap-northeast-1.amazonaws.com/pochi-sozai/nyan-bunch/fox.mp3')).play();
+  },
   abarenbo:function(){
     document.body.appendChild(new Audio('https://s3-ap-northeast-1.amazonaws.com/pochi-sozai/nyan-bunch/abarenbo.mp3')).play();
   },
@@ -16,24 +19,9 @@ var musics = {
   }
 };
 
-//function nyanMusic(num){
-//  switch (num) {
-//    case 'samba':
-//      console.log('=====================================');
-//      console.log(num);
-//      musics.samba();
-//      break;
-//    default:
-//      console.log('クエリに曲名入れて音楽ながしてちょ=====================================');
-//      console.log(num);
-//      break;
-//  }
-//}
-
 window.addEventListener('load', function(){
-  var sceneQuery = document.location.search.substr(1).split('scene=')[1];
-  var musicName = sceneQuery ? sceneQuery.split('&')[0] : 0;
-  //nyanMusic(musicName);
+  var sceneQuery = document.location.search.substr(1).split('scene=')[1],
+      musicName = sceneQuery ? sceneQuery.split('&')[0] : 0;
   musics[musicName] ? musics[musicName]() : console.log('クエリに曲名入れて音楽ながしてちょ=====================================');
 });
 
